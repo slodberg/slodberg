e6b979cf36651c09297ebc2099ddeb217b42ff63
eigi0088 <eigi0088@gmail.com>
2019-12-17 21:24:44 +0100

    meget færdig, mangler størrelse på BG

--- js.js
+++ js.js
@@ -1,13 +1,1 @@
-var window_width = $(window).width() - $(".smooth_scroll").width();
 
-var document_height = $(document).height() - $(window).height();
-
-$(function () {
-  $(window).scroll(function () {
-    var scroll_position = $(window).scrollTop();
-    var object_position_top = window_width * (scroll_position / document_height) * .2;
-    $(".smooth_scroll").css({
-      'top': object_position_top
-    });
-  });
-});
